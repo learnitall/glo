d8801b8b74e4f5be0d633e1b86243d725a0b0af5
Ryan Drew <learnitall0@gmail.com>
2021-05-01 17:21:24 -0600

    Add imports on features

--- glo/features/__init__.py
+++ glo/features/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Tools for working with features of datasets."""
-from . import allergen, indicator, nutrition, price, serving
+from . import allergen, indicator, nutrition, price, serving  # noqa: F401
